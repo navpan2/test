5765864d69a8fc1f6b7a526c0da8812ec69b474f
navpan2 <150070514+navpan2@users.noreply.github.com>
2023-11-23 13:15:59 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,7 +1,10 @@
 from stem import Signal
 from stem.control import Controller
+import os
 
 with Controller.from_port(port=9051) as controller:
     controller.authenticate()
     controller.signal(Signal.NEWNYM)
 
+a=os.system("torify curl http://icanhazip.com/")
+print(a)
