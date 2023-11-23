2390d937f1dc393a640515f2adf40b7ee6def782
navpan2 <150070514+navpan2@users.noreply.github.com>
2023-11-23 13:18:49 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,10 +1,18 @@
 from stem import Signal
 from stem.control import Controller
-import os
 
-with Controller.from_port(port=9051) as controller:
-    controller.authenticate()
-    controller.signal(Signal.NEWNYM)
+def change_identity():
+    try:
+        with Controller.from_port(port=9051) as controller:
+            controller.authenticate()
+            controller.signal(Signal.NEWNYM)
+        print("Identity changed successfully.")
+    except Exception as e:
+        print(f"Error: {e}")
 
-a=os.system("torify curl http://icanhazip.com/")
-print(a)
+if __name__ == "__main__":
+    change_identity()
+    a=os.system("torify curl http://icanhazip.com/")
+    print(a)
+
+
